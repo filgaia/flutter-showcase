e1df2348957befa537eee05bf3aff6ef4d30ffde
Filgaia <alejandroasp@gmail.com>
2022-05-03 16:16:12 -0500

    feature - Add meal item & upgrade gradle

---
+++ meals/lib/screens/category_meals_screen.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+
+import '../widgets/meal_item.dart';
+import '../data/dummy_data.dart';
+
+class CategoryMealsScreen extends StatelessWidget {
+  static const routeName = '/category-meals';
+
+  // const CategoryMealsScreen(this.categoryId, this.categoryTitle, {Key? key})
+  //    : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final routeArgs =
+        ModalRoute.of(context)?.settings.arguments as Map<String, String>;
+    final categoryId = routeArgs['id'];
+    final categoryTitle = routeArgs['title']!;
+    final categoryMeals = DUMMY_MEALS
+        .where((meal) => meal.categories.contains(categoryId))
+        .toList();
+
+    return Scaffold(
+      // appBar: AppBar(title: Text(categoryTitle)),
+      appBar: AppBar(title: Text(categoryTitle)),
+      body: ListView.builder(
+        itemBuilder: (ctx, index) => MealItem(
+          title: categoryMeals[index].title,
+          affordability: categoryMeals[index].affordability,
+          complexity: categoryMeals[index].complexity,
+          duration: categoryMeals[index].duration,
+          imageUrl: categoryMeals[index].imageUrl,
+        ),
+        itemCount: categoryMeals.length,
+      ),
+    );
+  }
+}
