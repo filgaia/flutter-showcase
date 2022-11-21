9b10bcaaf7bab4c173a4397739b54874608a9eb2
Filgaia <alejandroasp@gmail.com>
2022-11-21 11:41:28 -0500

    feature - add cart

--- shop/lib/main.dart
+++ shop/lib/main.dart
@@ -4,16 +4,24 @@ import 'package:provider/provider.dart';
 import './screens/products_overview_screen.dart';
 import './screens/product_detail_screen.dart';
 import './providers/products.dart';
+import './providers/cart.dart';
 
-void main() => runApp(const MyApp());
+void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return ChangeNotifierProvider(
-      create: (ctx) => Products(),
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider(
+          create: (ctx) => Products(),
+        ),
+        ChangeNotifierProvider(
+          create: (ctx) => Cart(),
+        ),
+      ],
       child: MaterialApp(
           title: 'MyShop',
           theme: ThemeData(
@@ -21,9 +29,9 @@ class MyApp extends StatelessWidget {
                 .copyWith(secondary: Colors.deepOrange),
             fontFamily: 'Lato',
           ),
-          home: const ProductsOverviewScreen(),
+          home: ProductsOverviewScreen(),
           routes: {
-            ProductDetailScreen.routeName: (ctx) => const ProductDetailScreen(),
+            ProductDetailScreen.routeName: (ctx) => ProductDetailScreen(),
           }),
     );
   }
